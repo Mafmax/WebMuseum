785cefba552feb505c743e4d04cb7f2bda0dad0f
Mafmax <61727763+Mafmax@users.noreply.github.com>
2021-02-07 10:56:09 +0500

    Interior completed. Moving completed.

---
+++ Museum/Assets/Scripts/LabelMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabelMove : MonoBehaviour
+{
+    [Range(2, 20)]
+    public float speed = 2;
+    [Range(0.25f, 2)]
+    public float amplitude = 0.25f;
+
+    public Transform person;
+
+    private float time = 0;
+    private Transform label;
+    private Vector3 startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        label = transform;
+        startPosition = transform.position;
+        Debug.LogError(label.forward);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        label.forward = new Vector3(person.position.x, 0, person.position.z) - label.position;
+        time += Time.deltaTime;
+        label.position = startPosition + new Vector3(0, amplitude * Mathf.Cos(speed * time), 0);
+    }
+}
---
+++ Museum/Assets/Scripts/MoveController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    public float sensitivity = 1f;
+    
+    public float minimumVert = -45.0f;
+    public float maximumVert = 45.0f;
+
+    private float _rotationX = 0;
+    private CharacterController controller;
+    private float playerSpeed = 20.0f;
+    private Camera mainCam;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        controller = gameObject.AddComponent<CharacterController>();
+        mainCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        _rotationX -= Input.GetAxis("Mouse Y") * sensitivity;
+        _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+        float delta = Input.GetAxis("Mouse X") * sensitivity;
+        float rotationY = transform.localEulerAngles.y + delta;
+        transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        Vector3 frontBackMove = new Vector3(mainCam.transform.forward.x, 0, mainCam.transform.forward.z);
+        Vector3 leftRightMove = new Vector3(frontBackMove.z, 0, -frontBackMove.x);
+        controller.Move(frontBackMove * Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed);
+        controller.Move(leftRightMove * Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed);
+
+    }
+}
